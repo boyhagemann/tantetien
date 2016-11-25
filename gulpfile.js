f66f872b0ef00e25d481708daf1ce296f18f6781
boyhagemann <boyhagemann@gmail.com>
2016-11-25 16:38:50 +0100

    updated html and styles

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var htmlmin = require('gulp-htmlmin');
+var uncss = require('gulp-uncss');
+var rename = require("gulp-rename");
+var nano = require('gulp-cssnano');
 
 gulp.task('sass', function () {
   return gulp.src('./src/sass/**/*.scss')
@@ -11,6 +15,36 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./docs/css'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./src/sass/**/*.scss', ['sass']);
+gulp.task('css', function () {
+    return gulp.src('./docs/css/style.css')
+        .pipe(uncss({
+            html: ['./docs/index.html']
+        }))
+        .pipe(nano())
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('./docs/css'));
 });
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/sass/**/*.scss', ['sass', 'css']);
+});
+
+gulp.task('fonts', function() {
+    gulp.src('./src/fonts/**/*')
+    .pipe(gulp.dest('./docs/fonts'));
+});
+
+gulp.task('html', function() {
+  return gulp.src('src/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./docs'));
+});
+
+gulp.task('html:watch', function () {
+  gulp.watch('./src/**/*.html', ['html']);
+});
+
+gulp.task('watch', ['sass:watch', 'html:watch']);
+
+
+gulp.task('default', ['html', 'sass', 'css', 'fonts']);
